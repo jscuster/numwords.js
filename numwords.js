c7794ca8556b6b4401aa358d5ba999d7bce31fff
Jason Custer <jscuster@Jasons-MacBook-Pro.local>
2016-05-08 01:32:43 -0700

    First commit.

---
+++ numwords.js
@@ -0,0 +1,71 @@
+var number2Words = (function(x) {
+var sounds = {},
+ones = [],
+tens = [],
+groups = [],
+allReady = false,
+
+prepareWords = function() {
+names = "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty thirty fourty fifty sixty seventy eighty ninety thousand million billion".split(" ");
+//set up ones.
+for (var i = 0; i < 19; i++) {
+ones[i] = names[i];
+} //i
+//set up tens
+for (var i = 19; i < 27; i++) {
+tens[i-19] = names[i];
+} //i
+//groups: thousand, million, billion
+for (var i = 27; i < names.length; i++) {
+groups[i-27] = names[i];
+} //i
+}, //prepareWords
+
+splitNumber = function(x) {
+var res = [],
+i, j, ptr;
+x=(x+"").split("");
+while (x.length % 3 !== 0) x.unshift("0");
+ptr = x.length - 1;
+for (i = 0; ptr >= 0; i++) {
+res[i] = [];
+for (j = 2; j >= 0; j--) {
+res[i][j] = x[ptr--] * 1;
+} //j
+} //i
+return res;
+}, //splitNumber
+
+get3Words = function(x) {
+var r = "";
+if (x[0] > 0) r=r + ones[x[0]-1] + " hundred";
+if (x[1] === 1) {
+r = r + " " + ones[x[2] + 9];
+} else {
+if (x[1] > 1) r = r + " " + tens[x[1] - 2];
+if (x[2] > 0) r = r + " " + ones[x[2]-1];
+} //if
+return r;
+}, //get3Words
+
+getWords = function(x, skipPunctuation) {
+x = splitNumber(x);
+var ptr = -1,
+tmp,
+r,
+punc = ",";
+if (skipPunctuation) punc = "";
+r="";
+for (i = 0; i < x.length; i++) {
+tmp = get3Words(x[i]);
+if (ptr >= 0) tmp = tmp + " " + groups[ptr] + punc;
+ptr++;
+if (ptr >= groups.length) ptr--;
+r = tmp + " " + r;
+} //i
+return r;
+}; //getWords
+
+prepareWords();
+return getWords;
+})(); //number2Words
